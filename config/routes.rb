89acbf7688b4ca728ca7f4be546efb841743585e
skando <skando@sonix.asia>
2016-04-07 13:58:19 +0900

    it can be displayed top page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,3 @@
-#Rails.application.routes.draw do
-
-#  resources :projects do
-#    resources :tasks, only: [:create, :destroy]
-#  end
-
-#post'/projects/:project_id/tasks/:id/toggle' => tasks#toggle'
-
-#root 'projects#index'
-
-
 Origapp::Application.routes.draw do
 
   resources :projects do
--- db/migrate/20160406013418_create_tasks.rb
+++ db/migrate/20160406013418_create_tasks.rb
@@ -1,4 +1,4 @@
-class CreateProjects < ActiveRecord::Migration
+class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.string :title
