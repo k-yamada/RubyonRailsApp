3c1465ccd611f0a8b479f1e441db6b70312e45da
skando <skando@sonix.asia>
2016-04-12 18:39:03 +0900

    リスト→テーブル。社員の削除が出来なくなった...？

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
-    @projects = Project.all
+    @projects = Project.order(stuffnum: :asc)
   end
 
   def show; end
@@ -45,5 +45,4 @@ class ProjectsController < ApplicationController
   def set_project
     @project = Project.find(params[:id])
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -35,14 +35,4 @@ ActiveRecord::Schema.define(version: 20160408035318) do
 
   add_index "stuffs", ["project_id"], name: "index_stuffs_on_project_id"
 
-  create_table "tasks", force: :cascade do |t|
-    t.string   "title"
-    t.boolean  "done",       default: false
-    t.integer  "project_id"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-  end
-
-  add_index "tasks", ["project_id"], name: "index_tasks_on_project_id"
-
 end
